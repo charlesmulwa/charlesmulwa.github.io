356512f66e08892d452775aef783b4f78d930438
Charles Mulwa <jetobichals04@gmail.com>
2021-02-14 11:58:11 -0800

    My new files

--- java.js
+++ java.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -57,7 +57,7 @@ $(document).ready(function(){
         responsive: {
             0:{
                 items: 1,
-                nav: false
+                nav: true
             },
             600:{
                 items: 2,
